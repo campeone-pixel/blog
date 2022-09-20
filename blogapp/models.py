bcfc5db1f33e9aefa775dab14df2f71e21e3e69b
campeone-pixel <posesmatiasabel@gmail.com>
2022-09-20 19:42:27 -0300

    cree las plantillas y la carpeta static

---
+++ blogapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class BlogPost(models.Model):
+  pass
+
+class BlogComment(models.Model):
+  pass
+
+class BlogCategory(models.Model):
+  pass
+
+class BlogTag(models.Model):
+  pass
+
