0e1315059856e1f9c0e86cb49351ed3478d249cf
campeone-pixel <posesmatiasabel@gmail.com>
2022-10-04 23:08:46 -0300

    se agregaron mensajes por si no hay posts para mostrar

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,24 @@
-
-from unicodedata import category
 from django.shortcuts import render
 from blogapp.forms import Formulario_comment
 from blogapp.models import *
 from django.db.models import Count
 from django.contrib.auth.models import User
 from users.models import Profile
-from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
-from django.contrib.auth import login,logout,authenticate
 from .forms import *
 from django.shortcuts import redirect
-from django.contrib import messages
-
-
-
 
 
 def datos_sidebar():
     ultimos_posteos = Post.objects.order_by("fecha_creacion")[0:3]
     tags = Tag.objects.all()
     categorias = Category.objects.all().annotate(posts_count=Count("posts"))
-    inicio="inicio"
-    return {"ultimos_posteos": ultimos_posteos, "tags": tags, "categorias": categorias,'inicio':inicio}
+    inicio = "inicio"
+    return {
+        "ultimos_posteos": ultimos_posteos,
+        "tags": tags,
+        "categorias": categorias,
+        "inicio": inicio,
+    }
 
 
 # ----------------------------------------------------------------------#
@@ -30,8 +27,8 @@ def datos_sidebar():
 
 
 def inicio(request):
-    all_posts = Post.objects.all()
-    contexto = {"all_posts": all_posts}
+    todos_los_posts = Post.objects.all()
+    contexto = {"todos_los_posts": todos_los_posts}
     contexto.update(datos_sidebar())
     return render(request, "home.html", contexto)
 
@@ -39,21 +36,36 @@ def inicio(request):
 def articulos_por_categoria(request, slug):
     categoria_a_filtrar = Category.objects.get(slug=slug)
     articulos_por_categoria = Post.objects.filter(categoria=categoria_a_filtrar)
-    contexto = {
-        "articulos_por_categoria": articulos_por_categoria,
-    }
-    contexto.update(datos_sidebar())
-    return render(request, "home.html", contexto)
+    if articulos_por_categoria.count() == 0:
+        mensajes = "No hay articulos correspondiente a la categoria seleccionada"
+        contexto = {
+            "articulos_por_categoria": articulos_por_categoria,
+            "mensajes": mensajes,
+        }
+        contexto.update(datos_sidebar())
+        return render(request, "home.html", contexto)
+    else:
+        contexto = {
+            "articulos_por_categoria": articulos_por_categoria,
+        }
+        contexto.update(datos_sidebar())
+        return render(request, "home.html", contexto)
 
 
 def articulos_por_tag(request, slug):
     tag_a_filtrar = Tag.objects.get(slug=slug)
     articulos_por_tag = Post.objects.filter(tag=tag_a_filtrar)
-    contexto = {
-        "articulos_por_tag": articulos_por_tag,
-    }
-    contexto.update(datos_sidebar())
-    return render(request, "home.html", contexto)
+    if articulos_por_tag.count() == 0:
+        mensajes = "No hay articulos correspondiente con el tag seleccionado"
+        contexto = {"articulos_por_tag": articulos_por_tag, "mensajes": mensajes}
+        contexto.update(datos_sidebar())
+        return render(request, "home.html", contexto)
+    else:
+        contexto = {
+            "articulos_por_tag": articulos_por_tag,
+        }
+        contexto.update(datos_sidebar())
+        return render(request, "home.html", contexto)
 
 
 def articulos_buscados(request):
@@ -65,40 +77,55 @@ def articulos_buscados(request):
             contexto["articulos_buscados"] = articulos_buscados
             return render(request, "home.html", contexto)
         else:
-            contexto["message"] = "No se encontraron resultados."
+            contexto["mensajes"] = "No se encontraron resultados."
             return render(request, "home.html", contexto)
 
 
 def sobre_nosotros(request):
-  contexto= {'sobre_nosotros':'sobre_nosotros'}
-  return render(request, "home.html", contexto)
+    contexto = {"sobre_nosotros": "sobre_nosotros"}
+    return render(request, "home.html", contexto)
 
 
 def ver_articulo(request, slug):
     articulo = Post.objects.get(slug=slug)
-    
-    all_comments = Comment.objects.filter(post=articulo)
-    if request.method == 'POST':
-      form_comment= Formulario_comment(data=request.POST)
-      if form_comment.is_valid():
-        form_data= form_comment.cleaned_data
-        contenido= form_data.get('contenido')
-        new_comment= Comment.objects.create(post = articulo,contenido = contenido,autor=request.user)
-        new_comment.save()
-        form=Formulario_comment()
-        return render(request, "home.html", {"articulo": articulo,'all_comments':all_comments, 'form':form})
-      else:
-        form=Formulario_comment()
-        return render(request, "home.html", {"articulo": articulo,'all_comments':all_comments, 'form':form})
+
+    todos_los_comentarios = Comment.objects.filter(post=articulo)
+    if request.method == "POST":
+        form_comentario = Formulario_comment(data=request.POST)
+        if form_comentario.is_valid():
+            form_data = form_comentario.cleaned_data
+            contenido = form_data.get("contenido")
+            nuevo_comentario = Comment.objects.create(
+                post=articulo, contenido=contenido, autor=request.user
+            )
+            nuevo_comentario.save()
+            form = Formulario_comment()
+            return render(
+                request,
+                "home.html",
+                {"articulo": articulo, "todos_los_comentarios": todos_los_comentarios, "form": form},
+            )
+        else:
+            form = Formulario_comment()
+            return render(
+                request,
+                "home.html",
+                {"articulo": articulo, "todos_los_comentarios": todos_los_comentarios, "form": form},
+            )
 
     else:
-      form=Formulario_comment()
-      return render(request, "home.html", {"articulo": articulo,'all_comments':all_comments, 'form':form})
-
-def ver_profile(request,usuario):
-    
-    ver_usuario=User.objects.get(id=usuario)
-    ver_perfil=Profile.objects.get(user=usuario)
-    return render(request, 'home.html',{"ver_perfil":ver_perfil,'ver_usuario':ver_usuario})
+        form = Formulario_comment()
+        return render(
+            request,
+            "home.html",
+            {"articulo": articulo, "todos_los_comentarios": todos_los_comentarios, "form": form},
+        )
 
 
+def ver_perfil(request, usuario):
+
+    ver_usuario = User.objects.get(id=usuario)
+    ver_perfil = Profile.objects.get(user=usuario)
+    return render(
+        request, "home.html", {"ver_perfil": ver_perfil, "ver_usuario": ver_usuario}
+    )
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,61 +1,70 @@
-from contextlib import ContextDecorator
 from unicodedata import category
 from django.shortcuts import render
 from blogapp.models import *
-from django.db.models import Count
 from .forms import Formulario_post
 from django.shortcuts import redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
-@login_required(redirect_field_name='my_redirect_field')
+
+@login_required(redirect_field_name="login_request")
 def ver_posts(request):
-  all_posts = Post.objects.all()
-  form=Formulario_post()
-  return render(request,'crud.html',{'all_posts':all_posts,'form':form,'ver_posts':True})
+    all_posts = Post.objects.all()
+    form = Formulario_post()
+    return render(
+        request, "crud.html", {"all_posts": all_posts, "form": form, "ver_posts": True}
+    )
 
 
 def agregar_post(request):
-  if request.method=='POST':
-    form = Formulario_post(request.POST, request.FILES)
-    if form.is_valid():
-      form.save()
-      messages.success(request, 'El post fue guardado exitosamente.')
-      return redirect('ver_posts')
-      
+    if request.method == "POST":
+        form = Formulario_post(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "El post fue guardado exitosamente.")
+            return redirect("ver_posts")
+
+        else:
+            messages.error(
+                request,
+                "El post NO fue guardado exitosamente. Revise los datos enviados",
+            )
+
+            return redirect("ver_posts")
     else:
-      messages.error(request, 'El post NO fue guardado exitosamente. Revise los datos enviados')
+        return redirect("ver_posts")
 
-      return redirect('ver_posts')
-      
-def update_post(request,slug):
-  post=Post.objects.get(slug=slug)
-  if request.method== 'POST':
-    form = Formulario_post(request.POST, instance=post)
-    if form.is_valid():
-      form.save()
-      messages.success(request, 'El post fue editado exitosamente.')
-      return redirect('ver_posts')
-  else:
-    update_form=Formulario_post(instance=post)
-    return render(request, 'crud.html', {'update_form':update_form,'post':post})
 
-def delete_post(request,slug):
-  post_delete=Post.objects.get(slug=slug)
-  if request.method=='POST':
-    post_delete.delete()
-    return redirect('ver_posts')
-  else:
-    return render(request, 'crud.html', {'post_delete':post_delete})
+def actualizar_post(request, slug):
+    post = Post.objects.get(slug=slug)
+    if request.method == "POST":
+        form = Formulario_post(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "El post fue editado exitosamente.")
+            return redirect("ver_posts")
+    else:
+        update_form = Formulario_post(instance=post)
+        return render(request, "crud.html", {"update_form": update_form, "post": post})
 
-def search_posts(request):
+
+def borrar_post(request, slug):
+    post_delete = Post.objects.get(slug=slug)
+    if request.method == "POST":
+        post_delete.delete()
+        return redirect("ver_posts")
+    else:
+        return render(request, "crud.html", {"post_delete": post_delete})
+
+
+def buscar_posts(request):
     if request.method == "GET":
         busqueda = request.GET.get("s")
         search_post = Post.objects.filter(titulo__icontains=busqueda)
-        contexto={'ver_posts':True}
+        contexto = {"ver_posts": True}
         if search_post.count() != 0:
             contexto["all_posts"] = search_post
             return render(request, "crud.html", contexto)
         else:
-            messages.success(request, 'No se encontraron resultados segun la busqueda')
-            return redirect('ver_posts')
+            messages.success(request, "No se encontraron resultados segun la busqueda")
+            return redirect("ver_posts")
